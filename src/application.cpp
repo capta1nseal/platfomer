acfec776b9af754f5f2840df1619f5f4c5dd306e
capta1nseal <aidan@equestra.net>
2024-07-26 09:25:27 +0300

    Add empty application class.

---
+++ src/application.cpp
@@ -0,0 +1,9 @@
+#include "application.hpp"
+
+
+Application::Application()
+{
+}
+Application::~Application()
+{
+}
---
+++ src/application.hpp
@@ -0,0 +1,20 @@
+#ifndef _PLATFORMERAPPLICATION_
+#define _PLATFORMERAPPLICATION_
+
+
+/*
+Wrapper class for a 2D platforer.
+Should contain only initialization, activation and destruction of other components.
+*/
+class Application
+{
+public:
+    Application();
+    ~Application();
+
+private:
+
+};
+
+
+#endif
