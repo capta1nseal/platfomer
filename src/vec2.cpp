825258561f947b994bc867da75efb94306289c2b
capta1nseal <aidan@equestra.net>
2024-08-17 18:33:55 +0300

    Fill out arithmetic operator overrides for Vec2.

--- src/vec2.cpp
+++ src/vec2.cpp
@@ -1,11 +1,103 @@
 #include "vec2.hpp"
 
 
-Vec2::Vec2()
+Vec2::Vec2(double x__, double y__)
 {
-    x = 0.0;
-    y = 0.0;
+    x = x__;
+    y = y__;
 }
 Vec2::~Vec2()
 {
+}
+
+bool Vec2::operator==(const Vec2& other) const
+{
+    return x == other.x && y == other.y;
+}
+
+Vec2 Vec2::operator+(const Vec2& other) const
+{
+    return Vec2(x + other.x, y + other.y);
+}
+Vec2 Vec2::operator+(double scalar) const
+{
+    return Vec2(x + scalar, y + scalar);
+}
+
+Vec2 Vec2::operator-(const Vec2& other) const
+{
+    return Vec2(x - other.x, y - other.y);
+}
+Vec2 Vec2::operator-(double scalar) const
+{
+    return Vec2(x - scalar, y - scalar);
+}
+Vec2 Vec2::operator-() const
+{
+    return Vec2(-x, -y);
+}
+
+double Vec2::operator*(const Vec2& other) const
+{
+    return x * other.x + y * other.y;
+}
+Vec2 Vec2::operator*(double scalar) const
+{
+    return Vec2(x * scalar, y * scalar);
+}
+Vec2 Vec2::operator/(double scalar) const
+{
+    return Vec2(x / scalar, y / scalar);
+}
+
+Vec2& Vec2::operator+=(const Vec2& other)
+{
+    x += other.x;
+    y += other.y;
+    return *this;
+}
+Vec2& Vec2::operator+=(double scalar)
+{
+    x += scalar;
+    y += scalar;
+    return *this;
+}
+
+Vec2& Vec2::operator-=(const Vec2& other)
+{
+    x -= other.x;
+    y -= other.y;
+    return *this;
+}
+Vec2& Vec2::operator-=(double scalar)
+{
+    x -= scalar;
+    y -= scalar;
+    return *this;
+}
+
+Vec2& Vec2::operator*=(const Vec2& other)
+{
+    x *= other.x;
+    y *= other.y;
+    return *this;
+}
+Vec2& Vec2::operator*=(double scalar)
+{
+    x *= scalar;
+    y *= scalar;
+    return *this;
+}
+
+Vec2& Vec2::operator/=(const Vec2& other)
+{
+    x /= other.x;
+    y /= other.y;
+    return *this;
+}
+Vec2& Vec2::operator/=(double scalar)
+{
+    x /= scalar;
+    y /= scalar;
+    return *this;
 }
--- src/vec2.hpp
+++ src/vec2.hpp
@@ -2,13 +2,38 @@
 #define _PLATFORMERVEC2_
 
 
-struct Vec2
+class Vec2
 {
 public:
     double x, y;
 
-    Vec2();
+    Vec2(double x = 0.0, double y = 0.0);
     ~Vec2();
+
+    bool operator==(const Vec2& other) const;
+
+    Vec2 operator+(const Vec2& other) const;
+    Vec2 operator+(double scalar) const;
+
+    Vec2 operator-(const Vec2& other) const;
+    Vec2 operator-(double scalar) const;
+    Vec2 operator-() const;
+
+    double operator*(const Vec2& other) const;
+    Vec2 operator*(double scalar) const;
+    Vec2 operator/(double scalar) const;
+
+    Vec2& operator+=(const Vec2& other);
+    Vec2& operator+=(double scalar);
+
+    Vec2& operator-=(const Vec2& other);
+    Vec2& operator-=(double scalar);
+    // mMltiplicative assignment is element-wise multiplication.
+    Vec2& operator*=(const Vec2& other);
+    Vec2& operator*=(double scalar);
+    // Divisive assignment is element-wise division.
+    Vec2& operator/=(const Vec2& other);
+    Vec2& operator/=(double scalar);
 
 private:
 
